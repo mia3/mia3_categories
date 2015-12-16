e8941ee79c6d6bbaf9c849ec27fef49eeb4a3637
Marc Neuhaus <mneuhaus@famelo.com>
2015-12-17 00:05:26 +0100

    changed package key

--- ext_tables.php
+++ ext_tables.php
@@ -7,14 +7,14 @@ if (TYPO3_MODE === 'BE') {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
         'Mia3.' . $_EXTKEY,
         'web',          // Main area
-        'mod1',         // Name of the module
+        'txmia3categoriesmod1',         // Name of the module
         '',             // Position of the module
         array(          // Allowed controller action combinations
             'Category' => 'index,updateSorting'
         ),
         array(          // Additional configuration
             'access'    => 'user,group',
-            'icon'      => 'EXT:mia3_categories/ext_icon.gif',
+            'icon'      => 'EXT:core/Resources/Public/Icons/T3Icons/mimetypes/mimetypes-x-sys_category.svg',
             'labels'    => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod.xml',
         )
     );
